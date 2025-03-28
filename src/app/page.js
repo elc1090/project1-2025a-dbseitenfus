123ae5638d4c5073d418a5cc92d5efecf65b25e7
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-03-28 17:12:46 -0300

    feat: create HomeProductsSection

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import HomeCarousel from "@/components/HomeCarousel";
+import HomeProductsSection from "@/components/HomeProductsSection";
 
 export default function Home() {
   return (
     <div>
       <main className="flex justify-center">
-        <HomeCarousel />
+        <div className="bg-black flex flex-col justify-center container items-center">
+          <HomeCarousel />
+
+          <HomeProductsSection />
+        </div>
         
       </main>
     </div>
---
+++ src/components/HomeProductsSection.jsx
@@ -0,0 +1,14 @@
+export default function HomeProductsSection() {
+    return (
+        <div className="bg-black flex flex-col w-full"> {/* Ajustado a altura mínima */}
+            <div className="bg-orange-500 flex justify-between items-center w-full h-[66px] pr-[32px] pl-[32px]">
+                <h2 className="text-white text-[20px]"> <strong> FESTIVAL DE OFERTAS</strong></h2>
+                <h2 className="text-white text-[20px]"> <strong> TERMINA EM: 3 DIAS</strong></h2>
+            </div>
+
+            <div className="bg-green-500 w-full h-[66px]">
+                
+            </div>
+        </div>
+    )
+}
