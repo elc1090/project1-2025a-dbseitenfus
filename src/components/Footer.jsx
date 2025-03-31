c0cb9518e262f024375b73070386d00d1ecc445a
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-03-31 16:29:00 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,7 +13,6 @@ const selos = [
 
 export default function Footer() {
 
-
     const footerList = {
         "Departamentos": [
             "Hardware",
@@ -68,23 +67,22 @@ export default function Footer() {
             "Meus Pedidos",
             "Cadastrar ou Remover Newsletter"
         ],
-"Atendimento": [
-    "Horário de atendimento:",
-    "08:00 às 20:00 - Segunda a Sexta,",
-    "09:00 às 15:00 - Sábado,",
-    "horário de Brasília",
-    "(Exceto domingo e feriados)",
-    "Endereço:",
-    "Rua Carlos Gomes, 1321 -",
-    "9° andar - Centro",
-    "Limeira/SP - CEP: 13480-010"
-]
-
+        "Atendimento": [
+            "Horário de atendimento:",
+            "08:00 às 20:00 - Segunda a Sexta,",
+            "09:00 às 15:00 - Sábado,",
+            "horário de Brasília",
+            "(Exceto domingo e feriados)",
+            "Endereço:",
+            "Rua Carlos Gomes, 1321 -",
+            "9° andar - Centro",
+            "Limeira/SP - CEP: 13480-010"
+        ]
     }
 
     return (
         <footer className="mt-5">
-            <div className="bg-blue-800 w-full flex text-white p-7 flex justify-between items-center">
+            <div className="bg-blue-strong w-full text-white p-7 flex justify-between items-center">
                 <div className="flex flex-col justify-start items-center">
                     <h1><b>KaBum! News</b></h1>
                     {/* <p>Receba ofertas</p> */}
@@ -102,12 +100,12 @@ export default function Footer() {
                     className="pl-10 h-[50px] bg-white ml-40 ml-10"
                 />
 
-                <Button className="bg-orange-600 w-[200px] h-[50px] ml-40 ml-10">
+                <Button className="bg-orange-weak w-[200px] h-[50px] ml-40 ml-10">
                     CADASTRAR
                 </Button>
                 
             </div>
-            <div className="bg-blue-700 w-full flex text-white p-7">
+            <div className="bg-blue-footer w-full flex text-white p-7">
                 {Object.keys(footerList).map((item, index) => (
                     <ul key={index} className="mr-10">
                         <p className="text-sm"><b>{item}</b></p>
@@ -124,13 +122,13 @@ export default function Footer() {
                         key={index}
                         src={selo.src} 
                         alt={selo.alt} 
-                        width={140} 
+                        width={125} 
                         height={80} 
                         objectFit="contain" 
                     />
                 ))}
             </div>
-            <div className="bg-blue-800 w-full  p-8 flex flex-col justify-center items-center text-white"> 
+            <div className="bg-blue-strong w-full  p-8 flex flex-col justify-center items-center text-white"> 
                 <p><b>Uma empresa do grupo MAGALU</b></p>
                 <p className="text-[10px] mt-5 text-center"><b>KaBuM!® é uma marca registrada de KABUM S.A | CNPJ: 05.570.714/0001-59 | Todos os direitos reservados. Os preços anunciados neste site ou via e-mail promocional podem ser alterados sem prévio aviso. O KaBuM! não é responsável por erros descritivos. As fotos contidas nesta página são meramente ilustrativas do produto e podem variar de acordo com o fornecedor/lote do fabricante. Este site trabalha 100% em criptografia SSL.  Clique aqui e veja as políticas de nossa empresa.</b></p>
             </div>
