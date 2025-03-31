cd7dbf60fca4cdf16681ad37b9d7ab3ef42389f1
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-03-31 18:55:15 -0300

    fix: top banner responsiveness

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -4,8 +4,12 @@ import TopBannerImage from "@/resources/top-banner.webp"
 
 export default function TopBanner() {
   return (
-    <div className="w-full">
-        <Image src={TopBannerImage} />
+    <div className="w-full h-[70px] relative">
+        <Image 
+          src={TopBannerImage}
+          alt="Top Banner"
+          fill
+          className="object-cover" />
     </div>
   )
 }
