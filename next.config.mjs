2475dc27e3f965a591ec98539f82268c0c65a4b9
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-03-31 18:18:45 -0300

    Revert "Update next.config.mjs"
    This reverts commit ecd572990e5c1f74399b2a12d117b450d1499d16.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Gera arquivos HTML estáticos
-    basePath: '/project1-2025a-dbseitenfus', // Substitua pelo nome do seu repositório no GitHub
-    images: {
-      unoptimized: true, // Desabilita otimização de imagens (necessário para export estático)
-    },
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
   };
   
-  export default nextConfig;
+  module.exports = nextConfig;
