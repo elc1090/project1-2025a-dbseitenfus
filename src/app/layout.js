6f0e169afc812d0a8ed5edfa494087798c50a885
Daniel Seitenfus <dbseitenfus@inf.ufsm.br>
2025-03-31 16:29:06 -0300

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,31 +1,30 @@
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+import TopBanner from "@/components/TopBanner";
 import Navbar from "@/components/NavBar";
 import HomeNavigationMenu from "@/components/HomeNavigationMenu";
 import Footer from "@/components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { Poppins } from "next/font/google";
 
 export const metadata = {
   title: "KaBum! Clone",
   description: "Clone do site KaBum! desenvolvido com Next.js",
 };
 
+// Importando a fonte Poppins com diferentes pesos
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], // Pesos que queremos carregar
+  display: "swap",
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={poppins.className}
       >
+        <TopBanner />
         <Navbar />
         <HomeNavigationMenu />
         {children}
